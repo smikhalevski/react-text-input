c2d71e55e48cdeb346738d15d825dc949d85b6b2
Savva Mikhalevski <smikhalevski@gmail.com>
2016-06-21 12:13:49 +0300

    Fixed assembly

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ var path = require('path'),
 module.exports = {
   entry: {
     index: [
-      './src/main/index.js',
-      './src/main/index.less'
+      './src/main/index.less',
+      './src/main/index.js'
     ]
   },
   output: {
     path: './target/out',
     filename: '[name].js',
-    libraryTarget: 'commonjs'
+    libraryTarget: 'commonjs2'
   },
   externals: [
     {
